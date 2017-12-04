10dc57b6cffd73a8637f3f5eb55e97fb3f8fb577
Jacqueline St Pierre <jstpier1@binghamton.edu>
2017-12-04 12:53:19 -0500

    Add files

---
+++ ControlPanel.java
@@ -0,0 +1,59 @@
+package projectview;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+
+public class ControlPanel implements Observer {
+
+	private ViewMediator mediator;
+	private JButton stepButton = new JButton("Step");
+	private JButton clearButton = new JButton("Clear");
+	private JButton runButton = new JButton("Run/Pause");
+	private JButton reloadButton = new JButton("Reload");
+	
+	public ControlPanel(ViewMediator med) {
+		mediator = med;
+		mediator.addObserver(this);
+	}
+	
+	public JComponent createControlDisplay() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1,0));
+		
+		stepButton.setBackground(Color.WHITE);
+		stepButton.addActionListener(e -> mediator.step());
+		panel.add(stepButton);
+		
+		clearButton.setBackground(Color.WHITE);
+		clearButton.addActionListener(e -> mediator.clear());
+		panel.add(clearButton);
+		
+		runButton.setBackground(Color.WHITE);
+		runButton.addActionListener(e -> mediator.toggleAutoStep());
+		panel.add(runButton);
+		
+		reloadButton.setBackground(Color.WHITE);
+		reloadButton.addActionListener(e -> mediator.reload());
+		panel.add(reloadButton);
+		
+		JSlider slider = new JSlider(5,1000);
+		slider.addChangeListener(e -> mediator.setPeriod(slider.getValue()));
+		panel.add(slider);
+		
+		return panel;
+	}
+	
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ StepControl.java
@@ -0,0 +1,36 @@
+package projectview;
+
+import javax.swing.Timer;
+
+
+public class StepControl {
+	private static final int TICK = 500;
+	private boolean autoStepOn = false;
+	private Timer timer;
+	private ViewMediator mediator;
+	
+	public StepControl(ViewMediator med) {
+		mediator = med;
+	}
+
+	boolean isAutoStepOn() {
+		return autoStepOn;
+	}
+
+	void setAutoStepOn(boolean autoStepOn) {
+		this.autoStepOn = autoStepOn;
+	}
+	
+	void toggleAutoStep() {
+		autoStepOn = !autoStepOn;
+	}
+	
+	void setPeriod(int period) {
+		timer.setDelay(period);
+	}
+	
+	void start() {
+		timer = new Timer(TICK, e -> {if(autoStepOn) mediator.step();});
+		timer.start();
+	}
+}
