4b1b879c27a3bcba398c1f91952f2c1da6489b16
asokolowski17 <asokolo4@binghamton.edu>
2017-12-06 14:46:13 -0500

    Add files from part 1

---
+++ Memory.java
@@ -0,0 +1,86 @@
+package project;
+
+import java.util.Arrays;
+
+public class Memory
+{
+	public static final int DATA_SIZE = 512;
+	public static final int CODE_SIZE = 256;
+	private int[] data = new int[DATA_SIZE];
+	private Instruction[] code = new Instruction[CODE_SIZE];
+	private int changedDataIndex = -1;
+	private int programSize = 0;
+	
+	
+	int[] getData()
+	{
+		return data;
+	}
+	
+	 int getData(int index)
+	 {
+		 return data[index]; 
+	 }
+	 
+	 void setData(int index, int value)
+	 {
+		 value = data[index];
+		 changedDataIndex = Math.max(changedDataIndex, index);
+	 }
+	 
+	 int getChangedDataIndex()
+	 {
+		 return changedDataIndex;
+	 }
+	 
+	 
+	 int getProgramSize()
+	 {
+		 return programSize;
+	 }
+	 
+	 void clearData()
+	 {
+		 Arrays.fill(data, 0);
+		 changedDataIndex = -1;
+	 }
+	 
+	 int[] getData(int min, int max)
+	 {
+		 return Arrays.copyOfRange(data, min, max);
+	 }
+	 
+	 Instruction[] getCode()
+	 {
+		 return code;
+	 }
+
+	 Instruction[] getCode(int min, int max)
+	 {
+		 return Arrays.copyOfRange(code, min, max);
+	 }
+	 
+	 Instruction getCode(int index)
+	 {
+		 return code[index];
+	 }
+
+	 void setCode(int index, Instruction value)
+	 {
+		 value = code[index];
+		 programSize = Math.max(programSize, index);
+	 }
+	 
+	 void clearCode()
+	 {
+		 code = null; 
+	 }
+	 
+	 void setProgramSize(int programSize)
+	 {
+		 this.programSize = programSize;
+	 }
+
+
+	
+}
